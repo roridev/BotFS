a449e5697a464d54c53f8fbaddc77e6a1861852e
roridev <t3ctotalmenterandom1@outlook.com>
2019-12-29 17:16:47 -0300

    Started working on the MySql Provider

---
+++ BotFS.MySql/MySqlDatabase.cs
@@ -0,0 +1,51 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotFS.MySql
+{
+    public class MySqlDatabase : IDatabase<MySqlProvider, MySqlConnection>
+    {
+        public string Name { get;set; }
+        public BaseServerProvider<MySqlConnection> Provider { get;set; }
+        public int Size { get;set; }
+        public List<string> Tables { get;set; }
+
+        public DBResponse<bool> Drop()
+        {
+            var response = new DBResponse<bool>
+            {
+                HasValue = false
+            };
+            if (this.Provider.Databases == null) this.Provider.Refresh();
+            if (this.Provider.Databases.Contains(this.Name))
+            {
+                this.Provider.Server.Execute($"drop database {this.Name};");
+                response.HasValue = true;
+                response.Response = true;
+            }
+            return response;
+        }
+
+        public DBResponse<List<string>> Refresh()
+        {
+            var response = new DBResponse<List<string>> 
+            {
+                HasValue = false
+            };
+            if (this.Provider.Server.Database != this.Name) this.Provider.Server.Execute($"use {this.Name};");
+            response.HasValue = true;
+            this.Tables = this.Provider.Server.Query<string>("show tables;").AsList<string>();
+            this.Size = this.Tables.Count;
+            response.Response = this.Tables;
+            return response;
+        }
+        public static implicit operator MySqlDatabase(DBResponse<IDatabase<MySqlProvider, MySqlConnection>> o)
+        {
+            return (MySqlDatabase)o.Response;
+        }
+    }
+    
+}
---
+++ BotFS.MySql/MySqlProvider.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace BotFS.MySql
+{
+    public class MySqlProvider : BaseServerProvider<MySqlConnection>
+    {
+        public MySqlConnection Server { get;set; }
+        public List<string> Databases { get;set; }
+        public ServerStatus Status { get;set; }
+
+        public DBResponse<List<string>> Refresh()
+        {
+            var response = new DBResponse<List<string>>
+            {
+                HasValue = false
+            };
+            if(this.Server != null)
+            {
+                if (this.Server.State != ConnectionState.Open) this.Server.Open();
+                response.HasValue = true;
+                response.Response = this.Server.Query<string>("show databases;").AsList<string>();
+            }
+            return response;
+        }
+    }
+}
