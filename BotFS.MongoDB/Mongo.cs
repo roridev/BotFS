12cb47de34b230ef17dbd6ff613168e2f6723efe
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-04-07 16:04:20 -0300

    Fixes bug

--- BotFS.MongoDB/Mongo.cs
+++ BotFS.MongoDB/Mongo.cs
@@ -1,81 +1,73 @@
 using MongoDB.Driver;
- using static BotFS.Utils.Logger;
 using System;
-using System.Collections.Generic;
+using static BotFS.Utils.Logger;
 
 namespace BotFS.MongoDB
 {
-    public class Mongo : IServerProvider<MongoProvider,MongoClient>
-    {
-        public string ConnectionString { get; set; }
-        public string Name { get; set; }
-        public BaseServerProvider<MongoClient> Provider {get;set;}
+	public class Mongo : IServerProvider<MongoProvider, MongoClient>
+	{
+		public string ConnectionString { get; set; } = "";
+		public string Name { get; set; } = "";
 
-        public Mongo(string connectionString, string name)
-        {
-            this.Name = name;
-            this.ConnectionString = connectionString;
-            this.Provider = this.TryConnect().Response;
-        }
+		public int Port { get; internal set; } = 27017;
+		public BaseServerProvider<MongoClient> Provider { get; set; } = new MongoProvider() { Status = ServerStatus.DISCONNECTED };
 
-        public DBResponse<IDatabase<MongoProvider, MongoClient>> GetDatabase(string Name)
-        {
-            if (this.Provider == null) return new DBResponse<IDatabase<MongoProvider, MongoClient>> { HasValue = false, Response = null };
-            var call = this.Provider.Refresh();
-            if (call.HasValue && this.Provider.Databases == null) this.Provider.Databases = call.Response;
-            else if (!call.HasValue && this.Provider.Databases == null) this.Provider.Databases = new List<string>();
-            var response = new DBResponse<IDatabase<MongoProvider, MongoClient>>
-            {
-                HasValue = true,
-                Response = this.Provider.Server.GetDatabase(Name).ToBFS(this.Provider)
-            };
-            return response;
-        }
-        
-        public DBResponse<BaseServerProvider<MongoClient>> TryConnect()
-        {
-            Log(this.Name,"Attemping to connect to database.");
-            this.Provider = this.Provider ?? new MongoProvider();
-            var response = new DBResponse<BaseServerProvider<MongoClient>>
-            {
-                HasValue = false
-            }; 
-                
-            try
-            {
-                var _server = new MongoClient(
-                    new MongoClientSettings
-                    {
-                        Server = new MongoServerAddress(this.ConnectionString),
-                        ClusterConfigurator = config => config.ConfigureCluster(
-                            settings => settings.With(serverSelectionTimeout: TimeSpan.FromSeconds(1))
-                            )
-                    }
-                    );
-                this.Provider.Server = _server;
-                MongoDatabase ping = (MongoDatabase)this.GetDatabase("local").Response;
-                var innerProvider = new MongoProvider
-                {
-                    Server = _server,
-                    Status = ServerStatus.CONNECTED
-                };
-                response.HasValue = true;
-                response.Response = innerProvider;
-                
-                Log(this.Name, "Connected.");
-            }
-            catch (TimeoutException)
-            {
-                
-                this.Provider.Status = ServerStatus.DISCONNECTED;
-                Warn(this.Name,"Couldn't connect to database.");
-            }
-            catch (ArgumentException)
-            {
-                this.Provider.Status = ServerStatus.INVALID;
-                Err(this.Name, "Invalid Connection String");
-            }
-            return response;
-        }
-    }
+		public Mongo(string connectionString, string name)
+		{
+			ConnectionString = connectionString;
+			Name = name;
+		}
+
+		public Mongo(string connectionString, int port, string name)
+		{
+			ConnectionString = connectionString;
+			Port = port;
+			Name = name;
+		}
+
+		public DBResponse<IDatabase<MongoProvider, MongoClient>> GetDatabase(string name) => Provider.Status != ServerStatus.CONNECTED
+				? new DBResponse<IDatabase<MongoProvider, MongoClient>> { HasValue = false }
+				: new DBResponse<IDatabase<MongoProvider, MongoClient>>
+				{
+					HasValue = true,
+					Response = Provider.Server.GetDatabase(name).ToBFS(Provider)
+				};
+
+		public DBResponse<BaseServerProvider<MongoClient>> TryConnect()
+		{
+			Log(Name, "Connecting...");
+			var response = new DBResponse<BaseServerProvider<MongoClient>>()
+			{
+				HasValue = false
+			};
+
+			try
+			{
+				var _server = new MongoClient(new MongoClientSettings
+				{
+					Server = new MongoServerAddress(ConnectionString),
+					ClusterConfigurator = config => config.ConfigureCluster(
+						settings => settings.With(serverSelectionTimeout: TimeSpan.FromSeconds(1))
+					)
+				});
+
+				_server.StartSession();
+				Provider.Server = _server;
+				Provider.Status = ServerStatus.CONNECTED;
+				Provider.Databases = _server.ListDatabaseNames().ToList();
+				response.Response = Provider;
+			}
+			catch (TimeoutException)
+			{
+				Provider.Status = ServerStatus.DISCONNECTED;
+				Warn(Name, "Conection Unreachable.");
+			}
+			catch (ArgumentException)
+			{
+				Provider.Status = ServerStatus.INVALID;
+				Warn(Name, "Invalid Connection String. Connection Canceled.");
+			}
+			return response;
+		}
+	}
 }
