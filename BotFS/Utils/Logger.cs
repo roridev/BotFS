956cd26589e6eb8c8d77ab28d862bb6717b7f841
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-04-07 18:15:19 -0300

    Modifies logger to be sisbase-like

--- BotFS/Utils/Logger.cs
+++ BotFS/Utils/Logger.cs
@@ -1,29 +1,39 @@
-﻿using BotFS;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+using stdout = System.Console;
+using color = System.ConsoleColor;
 
 namespace BotFS.Utils
 {
-    public static class Logger
-    {
-        public static void Log(string Name, string Message) 
-        {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine($"<{DateTime.Now.ToString("MMMM dd - HH:mm:ss")}> [BotFS] MongoDB | [{Name}] - {Message}");
-            Console.ResetColor();
-        }
-        public static void Warn(string Name, string Message)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.WriteLine($"<{DateTime.Now.ToString("MMMM dd - HH:mm:ss")}> [BotFS] MongoDB | [{Name}] - {Message}");
-            Console.ResetColor();
-        }
-        public static void Err(string Name, string Message)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine($"<{DateTime.Now.ToString("MMMM dd - HH:mm:ss")}> [BotFS] MongoDB | [{Name}] - {Message}");
-            Console.ResetColor();
-        }
-    }
+	public static class Logger
+	{
+		private static void WriteDate()
+		{
+			stdout.ResetColor();
+			stdout.Write($"\n <{DateTime.Now:MM/dd - HH:mm:ss}> ");
+		}
+
+		public static void Log(string source, string message)
+		{
+			WriteDate();
+			stdout.ForegroundColor = color.Cyan;
+			stdout.Write($" {source} | {message} \t");
+			stdout.ResetColor();
+		}
+
+		public static void Warn(string source, string message)
+		{
+			WriteDate();
+			stdout.ForegroundColor = color.Yellow;
+			stdout.Write($" {source} | {message} \t");
+			stdout.ResetColor();
+		}
+
+		public static void Err(string source, string message)
+		{
+			WriteDate();
+			stdout.ForegroundColor = color.Red;
+			stdout.Write($" {source} | {message} \t");
+			stdout.ResetColor();
+		}
+	}
 }
